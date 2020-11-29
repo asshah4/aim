fad5b9ecbb12f32aedd785da110bb1091813b5bc
Anish Shah <mrshahman@gmail.com>
2020-11-28 22:53:24 -0600

    corrected the parameters for specific aims

--- R/specific-aims.R
+++ R/specific-aims.R
@@ -2,6 +2,8 @@
 #'
 #' @description Every research project, particularly proposals, starts with a specific aims page, which contains the aims and sub-aims intended to be studied along with the corresponding hypothesis. The `specific_aims()` function is a method to collect all the related `aim` objects from a project to help track, update, and manage the research project.
 #'
+#' @param ... Variable number of `aim` objects. The name of the objects will be retained.
+#'
 #' @return An `aims` object that is a collection of the related individual `aim` objects from a project.
 #'
 #' @export
