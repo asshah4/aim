4a1436fdf760c31a3bc3f14c9f7e030643673f51
Anish Shah <asshah4@users.noreply.github.com>
2022-07-25 21:22:35 -0500

    removed old gt table approaches

--- tests/testthat/test-gt.R
+++ tests/testthat/test-gt.R
@@ -1,137 +1,3 @@
-test_that("sequential gt tables can be made ", {
-
-	f <- mpg ~ wt + hp + qsec + cyl + vs + gear
-	ptest <- stats::lm
-	exposures <- c("wt", "hp", "qsec")
-
-	h1 <-
-		hypothesize(
-			f,
-			exposures,
-			combination = "sequential",
-			test = ptest,
-			data = mtcars,
-			strata = "am"
-		)
-
-	x <-
-		create_models() %>%
-		add_hypothesis(h1) %>%
-		construct_tests() %>%
-		extract_results(how = "tidy") %>%
-		extract_results(how = "glance") %>%
-		flatten()
-
-	# Create a `gt` table successfully
-	y <-
-		tbl_sequential(
-			x,
-			terms = term ~ list("wt", "hp", "qsec"),
-			by = level ~ list("0" = "Manual", "1" = "Automatic"),
-			models = number ~ list(1:3),
-			model_label = "Model",
-			model_notes = list("Mileage", "Exposure", "Cylinders", "Gear"),
-			values = c("estimate", "conf.low", "conf.high", "r.squared"),
-			pattern = "{1} ({2}, {3}); R^2 = {4}",
-			statistic = p.value ~ 0.05
-		)
-
-	expect_s3_class(y, "gt_tbl")
-	expect_named(y[["_styles"]]$styles[[1]]$cell_fill, "color")
-
-	# Able to shrink the table
-	z <-
-		y %>%
-		theme_gt_compact(table.font.size = gt::px(12)) %>%
-		.[["_options"]] %>%
-		.[.$parameter == "table_font_size", ] %>%
-		.$value %>%
-		.[[1]]
-
-	expect_equal(z, "12px")
-
-})
-
-test_that("comparison gt tables can be made", {
-
-	h1 <- hypothesize(
-		x = mpg ~ hp + cyl,
-		exposures = "hp",
-		test = stats::lm,
-		combination = "direct",
-		data = mtcars
-	)
-
-	h2 <- hypothesize(
-		x = mpg ~ hp + am,
-		exposures = "hp",
-		test = stats::lm,
-		combination = "direct",
-		data = mtcars
-	)
-
-	expect_message({
-		x <-
-			create_models() %>%
-			add_hypothesis(h1) %>%
-			add_hypothesis(h2) %>%
-			construct_tests() %>%
-			extract_results(how = "tidy") %>%
-			flatten()
-	})
-
-	y <- tbl_compare(
-		data = x,
-		terms = term ~ list(hp = "Horsepower", cyl = "Cylinders", am = "Transmission"),
-		models = name ~ list(h1 = "First", h2 = "Second"),
-		statistic = p.value ~ 0.05,
-		values = c("estimate", "conf.low", "conf.high"),
-		pattern = "{1} ({2}, {3})",
-		style = fill ~ list(color = "lightgreen"),
-		decimals = 2,
-		missing_text = "-"
-	)
-
-	expect_s3_class(y, "gt_tbl")
-
-})
-
-test_that("gt tables can easily be made to represent multiple models", {
-
-	# Create models
-	hseq <- hypothesize(
-		x = mpg + qsec ~ wt + cyl + drat + am,
-		exposures = c("wt", "cyl"),
-		combination = "sequential",
-		test = stats::lm,
-		data = mtcars
-	)
-	hpar <- update_hypothesis(hseq, combination = "parallel")
-	maps <-
-		create_models() %>%
-		add_hypothesis(hseq) %>%
-		add_hypothesis(hpar) %>%
-		construct_tests() %>%
-		extract_results(how = "tidy")
-
-	object <- flatten(maps) %>% subset(., name == "hseq")
-
-
-	x = "exposures"
-	y = outcomes ~ list(mpg = "Mileage", qsec = "Acceleration")
-	id = "number"
-	terms = term ~ list("wt", "drat", am = "Transmission")
-	values = c("estimate", "conf.low", "conf.high")
-	pattern = "{1} ({2}, {3})"
-	statistic = p.value ~ 0.05
-	style = fill ~ list(color = "lightgreen")
-	decimals = 2
-	missing_text = "-"
-
-
-
-
-})
 
 test_that("subgroup models can be made, with a forest plot", {
 
@@ -152,20 +18,18 @@ test_that("subgroup models can be made, with a forest plot", {
 	fits <- fit(f, .fit = lm, data = test_data, archetype = TRUE)
 	m <- mdls(fits)
 
-
-	tbl <- tbl_forest(
+	tbl <- tbl_forest.forge(
 		object = m,
 		formula = mpg ~ wt,
 		groups = c("vs", "am"),
 		columns = list(beta ~ "Estimates", conf ~ "95% CI", n ~ "Number"),
-		axis = list(lab ~ "B (95% CI)", lim ~ c(0, 0.2), breaks ~ c(0, .01, .02, .05, .1), scale ~ "continuous")
+		axis = list(lab ~ "B (95% CI)", lim ~ c(0, 0.1), breaks ~ c(0, .02, .05, .1), scale ~ "continuous")
 	)
 
 	expect_s3_class(tbl, "gt_tbl")
 
 	# TODO modify how axes are made
 	# TODO Ability to relabel the levels of stratified groups
-	# TODO Passing axis information for a logarithm scale instead of linear
 
 })
 
