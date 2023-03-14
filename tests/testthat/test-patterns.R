391a89a2f5f4c7c8522e9cb9a66bf231dfa78d43
Anish Shah <asshah4@users.noreply.github.com>
2023-03-14 10:07:41 -0500

    handle simple sequential expansion

--- tests/testthat/test-patterns.R
+++ tests/testthat/test-patterns.R
@@ -1,8 +1,27 @@
-test_that("pattern expansion occurs in simple formulas", {
+test_that("Sequential pattern expansion works appropriately", {
 
-  # Sequential
-	x <- fmls(witch ~ wicked + west + green, pattern = "sequential")
+	# Simple fmls
+	x <- fmls(witch ~ .x(wicked) + west + green, pattern = "sequential")
+	y1 <- pattern_sequentially(x)
+	expect_s3_class(y1, "fmls")
+	expect_length(y1, 3)
+	y2 <- pattern_sequentially(fmls(witch ~ .x(wicked) + west + green))
+	y3 <- pattern_sequentially(witch ~ .x(wicked) + west + green)
+	expect_message(pattern_sequentially(witch ~ .x(wicked) + west + green))
+	expect_no_message(y2)
+	expect_equal(y1, y2)
+	expect_equal(y2, y3)
 
-	#
+	# Simple formula
+	x <- witch ~ wicked + west + green
+	y1 <- pattern_sequentially(x)
+	y2 <- pattern_sequentially(fmls(x))
+	expect_length(y1, 3)
+	expect_equal(y1, y2)
+
+	# TODO mediation
+
+	# TODO interaction
 
 })
+
