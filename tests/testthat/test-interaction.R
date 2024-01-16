714830fa5e5b754937be654ad9827cc312a1170e
Anish Shah <asshah4@users.noreply.github.com>
2024-01-16 15:53:06 -0600

    start looking at interaction #29

---
+++ tests/testthat/test-interaction.R
@@ -0,0 +1,20 @@
+test_that('interaction estimates can be made', {
+	# Survival model
+	library(survival)
+	dat <- na.omit(lung)
+
+	expect_message(
+		x <-
+			fmls(Surv(time, status) ~ .x(age) + .i(sex),
+					 pattern = 'direct') |>
+			fit(.fn = coxph, data = dat, raw = FALSE)
+	)
+
+	object <- model_table(by_sex = x)
+	expect_s3_class(object, 'mdl_tbl')
+	expect_equal(nrow(object), 1)
+
+
+
+
+})
