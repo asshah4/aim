062dee201a8399e1ea8e24ff94321ceef12e9b53
Anish S. Shah <asshah4@users.noreply.github.com>
2024-05-08 22:42:21 -0500

    this is difficult

--- R/articulation.R
+++ R/articulation.R
@@ -14,11 +14,29 @@ articulate <- function() {
 #' @noRd
 new_articulation <- function() {
   
+  # An articulation or joint here describes a relationship in context
+  # Descriptive elements: 
+  #   Variable type = categorical, binary, continuous
+  # Relationship attributes include:
+  #   Model estimates and statistics (e.g. estimates, CI, p-value)
+  #   Mean values by group (if applicable based on description type)
+  # Contexts include:
+  #   Adjustment = sequential or parallel adjustment status
+  #   Strata = stratifying variable, which in turn depends on level + dataset
+  #   Datasets = number of observations included
+  
+  context <- list(
+    adjustment_type = "pattern",
+    adjustment_number = n, # Essentially number of other covariates
+    strata_variable = "strata",
+    strata_level = c("level A", "level B"),
+    
+  )
+  
   new_rcrd(
     list(
-      "left" = left,
-      "right" = right,
-      "model" = model
+      relationship = relationship,
+      context = context,
     ),
     class = "articulation"
   )
